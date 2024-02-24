3f81f0b55d227ca4972e9459bf455ee2dd21d9a0
AhmedMahmoudMOD <ahmed.mahmoud.abdelnaby98@gmail.com>
2024-02-24 07:12:13 +0200

    modifications to Validators and Creating Upload MW

--- MiddleWares/Validations/ClassValidations.js
+++ MiddleWares/Validations/ClassValidations.js
@@ -2,14 +2,14 @@ const { body } = require("express-validator");
 
 exports.insertValidations=[
     body('_id').isInt({min:1}).withMessage("ID Should be A positive Integer"),
-    body("fullname").isString().withMessage("Name Should Be String").isAlpha("en-US").withMessage("Name Should Be Written In English With No Numbers or Special Characters")
+    body("fullname").isString().withMessage("Name Should Be String").isAlpha("en-US",{ignore:" "}).withMessage("Name Should Be Written In English With No Numbers or Special Characters")
     .isLength({min:3}).withMessage("Name Should Be at least 3 letters long"),
     
 ]
 
 exports.updateValidations=[
     body('_id').isInt({min:1}).withMessage("ID Should be A positive Integer"),
-    body("fullname").optional().isString().withMessage("Name Should Be String").isAlpha("en-US").withMessage("Name Should Be Written In English With No Numbers or Special Characters")
+    body("fullname").optional().isString().withMessage("Name Should Be String").isAlpha("en-US",{ignore:" "}).withMessage("Name Should Be Written In English With No Numbers or Special Characters")
     .isLength({min:3}).withMessage("Name Should Be at least 3 letters long"),
 ]
 
